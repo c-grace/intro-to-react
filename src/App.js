e80a569dd928c1f1b2d0e5745661ee2fc03c779b
c-grace <70161515+c-grace@users.noreply.github.com>
2023-11-27 23:18:37 -0600

    changed some stuff

--- src/App.js
+++ src/App.js
@@ -5,10 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Hello World</p>
+        <p>ʕ•ᴥ•ʔ</p>
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -16,7 +15,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
